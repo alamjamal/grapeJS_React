3b95356a8c51f0e6f306a523923448d47c9bbe7c
Altamas khan <khanaltamash7795@gmail.com>
2023-04-09 00:20:49 +0530

    new code

--- src/Header.js
+++ src/Header.js
@@ -5,7 +5,7 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
-// import MenuIcon from '@mui/icons-material/Menu';
+
 
 export default function ButtonAppBar() {
   return (
@@ -21,7 +21,7 @@ export default function ButtonAppBar() {
             aria-label="menu"
             sx={{ mr: 2 }}
           >
-            {/* <MenuIcon /> */}
+          
           </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             TeachAnda
